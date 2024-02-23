88271f45dfcf6bcce1cc6bc44c4e95d658e8a21f
UND34TAK3R <156216878+UND34TAK3R@users.noreply.github.com>
2024-02-23 08:39:19 -0800

    Add files via upload

---
+++ script_svg.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const regionInfo = document.getElementById("region-info");
+    const regions = document.querySelectorAll(".region");
+   
+    regions.forEach(region => {
+      region.addEventListener("mouseover", function () {
+        regionInfo.textContent = `You are hovering over a ${region.id}`;
+      });
+   
+      region.addEventListener("mouseout", function () {
+        regionInfo.textContent = "Hover over a region on the right picture to see information";
+      });
+    });
+  });
+   
